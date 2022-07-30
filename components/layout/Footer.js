59c62ad6a1d964118233c6fc8c30df185c66e5f7
Leo-paul <leopaulmartin@hotmail.fr>
2022-07-31 00:26:24 +0200

    fix footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -2,18 +2,13 @@ import React from "react";
 
 function Footer() {
 	return (
-		<footer>
-			<footer className="bg-gray-200 text-center lg:text-left">
-				<div
-					className="text-gray-700 text-center p-4"
-					style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-				>
-					© 2021 Copyright:
-					<a className="text-gray-800" href="https://tailwind-elements.com/">
-						Tailwind Elements
-					</a>
-				</div>
-			</footer>
+		<footer className="w-full text-center border-t border-grey p-4 pin-b">
+			<div className="text-gray-700 text-center p-4">
+				© 2022 Copyright:
+				<a className="text-gray-800" href="https://tailwind-elements.com/">
+					Teammood - Clone
+				</a>
+			</div>
 		</footer>
 	);
 }
