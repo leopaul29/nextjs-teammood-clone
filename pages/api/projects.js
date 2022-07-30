47db6e2f1767456e75cedc4f5469fb90969e78ef
Leo-paul <leopaulmartin@hotmail.fr>
2022-07-30 20:15:49 +0200

    fix project api

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,16 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import prisma from '../../lib/prisma.js';
+import prisma from "../../lib/prisma.js";
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const data = await prisma.project.findMany({});
-      return res.status(200).json({ data });
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({ msg: 'Something went wrong' });
-    }
-  } else {
-    return res.status(405).json({ msg: 'Method not allowed' });
-  }
+	if (req.method === "GET") {
+		try {
+			let data;
+			console.log("req", req.query);
+			const { id } = req.query;
+			if (id) {
+				data = await prisma.project.findUnique({
+					where: {
+						id: +id,
+					},
+				});
+			} else {
+				data = await prisma.project.findMany({});
+			}
+
+			return res.status(200).json({ data });
+		} catch (err) {
+			console.error(err);
+			return res.status(500).json({ msg: "Something went wrong" });
+		}
+	} else {
+		return res.status(405).json({ msg: "Method not allowed" });
+	}
 }
