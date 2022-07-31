ea437c46ac6dd9962e241f6e4652d7377623f5cf
Leo-paul <leopaulmartin@hotmail.fr>
2022-08-01 00:04:48 +0200

    mention planetscale and hashnode in footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -4,10 +4,15 @@ function Footer() {
 	return (
 		<footer className="w-full text-center border-t border-grey p-4 pin-b">
 			<div className="text-gray-700 text-center p-4">
-				© 2022 Copyright:
-				<a className="text-gray-800" href="https://tailwind-elements.com/">
-					Teammood - Clone
-				</a>
+				© 2022 Copyright: Teammood [Clone] -{" "}
+				<a href="https://planetscale.com/?utm_source=hashnode&utm_medium=hackathon&utm_campaign=announcement_article">
+					PlanetScale
+				</a>{" "}
+				and{" "}
+				<a href="https://hashnode.com/?source=planetscale_hackathon_announcement">
+					Hashnode
+				</a>{" "}
+				hackathon
 			</div>
 		</footer>
 	);
