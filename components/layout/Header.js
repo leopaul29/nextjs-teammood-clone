1f0b87867c665fe514f9bbc98968d18dfb1a0ea5
Leo-paul <leopaulmartin@hotmail.fr>
2022-08-01 00:05:06 +0200

    fix header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,8 +1,7 @@
 import Link from "next/link";
 import React from "react";
-import ProjectMenu from "../ProjectMenu";
 
-function Header({ projects }) {
+export default function Header() {
 	return (
 		<header>
 			<nav
@@ -74,7 +73,6 @@ navbar navbar-expand-lg navbar-light
 									Dashboard
 								</Link>
 							</li>
-							{/* <ProjectMenu projects={projects} /> */}
 							<li className="nav-item pr-2">
 								<Link
 									className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0"
@@ -92,5 +90,3 @@ navbar navbar-expand-lg navbar-light
 		</header>
 	);
 }
-
-export default Header;
